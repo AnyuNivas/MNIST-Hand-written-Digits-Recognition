c63394e4d06bf062b9d8418b338a0d4d2cfab6cc
AnyuNivas <Srini@VENKATAs-MBP.midcoip.net>
2018-08-09 15:35:54 -0500

    files added

---
+++ main.js
@@ -0,0 +1,91 @@
+/*
+variables
+*/
+var model;
+var canvas;
+var classNames = [];
+var canvas;
+var coords = [];
+var mousePressed = false;
+var mode;
+/*
+get the current image data 
+*/
+function getImageData() {
+        //get the minimum bounding box around the drawing 
+        const mbb = getMinBox()
+
+        //get image data according to dpi 
+        const dpi = window.devicePixelRatio
+        const imgData = canvas.contextContainer.getImageData(mbb.min.x * dpi, mbb.min.y * dpi,
+                                                      (mbb.max.x - mbb.min.x) * dpi, (mbb.max.y - mbb.min.y) * dpi);
+        return imgData
+    }
+
+/*
+get the prediction 
+*/
+function getFrame() {
+    //make sure we have at least two recorded coordinates 
+    if (coords.length >= 2) {
+
+        //get the image data from the canvas 
+        const imgData = getImageData()
+
+        //get the prediction 
+        const pred = model.predict(preprocess(imgData)).dataSync()
+
+        //find the top 5 predictions 
+        const indices = findIndicesOfMax(pred, 5)
+        const probs = findTopValues(pred, 5)
+        const names = getClassNames(indices)
+
+        //set the table 
+        setTable(names, probs)
+    }
+
+}
+
+/*
+load the model
+*/
+async function start(cur_mode) {
+    //arabic or english
+    mode = cur_mode
+    
+    //load the model 
+    model = await tf.loadModel('model3/model.json')
+    
+    //warm up 
+    model.predict(tf.zeros([1, 28, 28, 1]))
+    
+    //allow drawing on the canvas 
+    allowDrawing()
+    
+    //load the class names
+    await loadDict()
+}
+/*
+allow drawing on canvas
+*/
+function allowDrawing() {
+    canvas.isDrawingMode = 1;
+    if (mode == 'en')
+        document.getElementById('status').innerHTML = 'Model Loaded';
+    else
+        document.getElementById('status').innerHTML = 'تم التحميل';
+    $('button').prop('disabled', false);
+    var slider = document.getElementById('myRange');
+    slider.oninput = function() {
+        canvas.freeDrawingBrush.width = this.value;
+    };
+}
+
+/*
+clear the canvs 
+*/
+function erase() {
+    canvas.clear();
+    canvas.backgroundColor = '#ffffff';
+    coords = [];
+}
